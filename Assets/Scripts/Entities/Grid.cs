4d017adbc6b3f186b7f62db8d4966b4f3d773a91
Emmanuel Vaccaro <emmanuel.vaccaro@gmail.com>
2019-06-21 14:58:03 +1000

    Slowly started adding mechanics to game

--- Assets/Scripts/Entities/Grid.cs
+++ Assets/Scripts/Entities/Grid.cs
@@ -7,14 +7,12 @@ namespace Minesweeper3D
     public class Grid : MonoBehaviour
     {
         public GameObject tilePrefab; // Prefab to spawn
-        public int width = 10, height = 10, depth = 10; // Grid dimensions
+        public int width = 10, height = 10; // Grid dimensions
         public int mineCount = 5;
         public float spacing = 1.1f; // Spacing between each tile
-        private Tile[,,] tiles; // 3D Array to store all the tiles
+        private Tile[,] tiles; // 3D Array to store all the tiles
 
-        
-
-        Tile SpawnTile(Vector3 position)
+        Tile SpawnTile(Vector2 position)
         {
             // Clone the tile prefab
             GameObject clone = Instantiate(tilePrefab, transform);
@@ -23,54 +21,51 @@ namespace Minesweeper3D
             // Return the Tile component of clone
             return clone.GetComponent<Tile>();
         }
+
         void GenerateTiles()
         {
             // Instantiate the new 3D array of size width x height x depth
-            tiles = new Tile[width, height, depth];
+            tiles = new Tile[width, height];
 
             // Store half the size of the grid
-            Vector3 halfSize = new Vector3(width * .5f, height * .5f, depth * .5f);
+            Vector2 halfSize = new Vector2(width * .5f, height * .5f);
 
             // Offset
-            Vector3 offset = new Vector3(.5f, .5f, .5f);
-            
+            Vector2 offset = new Vector2(.5f, .5f);
+
             // Loop through the entire list of tiles
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    for (int z = 0; z < depth; z++)
-                    {
-                        // Generate position for current tile
-                        Vector3 position = new Vector3(x - halfSize.x,
-                                                       y - halfSize.y,
-                                                       z - halfSize.z);
-                        // Offset position to center
-                        position += offset;
-                        // Apply spacing
-                        position *= spacing;
-                        // Spawn a new tile
-                        Tile newTile = SpawnTile(position);
-                        // Store coordinates
-                        newTile.x = x;
-                        newTile.y = y;
-                        newTile.z = z;
-                        // Store tile in array at those coordinates
-                        tiles[x, y, z] = newTile;
-                    }
+                    // Generate position for current tile
+                    Vector2 position = new Vector2(x - halfSize.x,
+                                                   y - halfSize.y);
+                    // Offset position to center
+                    position += offset;
+                    // Apply spacing
+                    position *= spacing;
+                    // Spawn a new tile
+                    Tile newTile = SpawnTile(position);
+                    // Store coordinates
+                    newTile.x = x;
+                    newTile.y = y;
+                    // Store tile in array at those coordinates
+                    tiles[x, y] = newTile;
                 }
             }
         }
+
+
         // Use this for initialization
         void Start()
         {
             GenerateTiles();
         }
-        bool IsOutOfBounds(int x, int y, int z)
+        bool IsOutOfBounds(int x, int y)
         {
             return x < 0 || x >= width ||
-                   y < 0 || y >= height ||
-                   z < 0 || z >= depth;
+                   y < 0 || y >= height;
         }
         int GetAdjacentMineCount(Tile tile)
         {
@@ -81,49 +76,45 @@ namespace Minesweeper3D
             {
                 for (int y = -1; y <= 1; y++)
                 {
-                    for (int z = -1; z <= 1; z++)
+                    // Calculate which adjacent tile to look at
+                    int desiredX = tile.x + x;
+                    int desiredY = tile.y + y;
+                    // Check if the desired x & y is outside bounds
+                    if (IsOutOfBounds(desiredX, desiredY))
                     {
-                        // Calculate which adjacent tile to look at
-                        int desiredX = tile.x + x;
-                        int desiredY = tile.y + y;
-                        int desiredZ = tile.z + z;
-                        // Check if the desired x & y is outside bounds
-                        if (IsOutOfBounds(desiredX, desiredY, desiredZ))
-                        {
-                            // Continue to next element in the loop
-                            continue;
-                        }
-                        // Select current tile
-                        Tile currentTile = tiles[desiredX, desiredY, desiredZ];
-                        // Check if that tile is a mine
-                        if (currentTile.isMine)
-                        {
-                            // Increase count by 1
-                            count++;
-                        }
+                        // Continue to next element in the loop
+                        continue;
+                    }
+                    // Select current tile
+                    Tile currentTile = tiles[desiredX, desiredY];
+                    // Check if that tile is a mine
+                    if (currentTile.isMine)
+                    {
+                        // Increase count by 1
+                        count++;
                     }
                 }
             }
             // Remember to return the count!
             return count;
         }
-        void FFuncover(int x, int y, int z, bool[,,] visited)
+        void FFuncover(int x, int y, bool[,] visited)
         {
             // Is x and y out of bounds of the grid?
-            if (IsOutOfBounds(x, y, z))
+            if (IsOutOfBounds(x, y))
             {
                 // Exit
                 return;
             }
 
             // Have the coordinates already been visited?
-            if (visited[x, y, z])
+            if (visited[x, y])
             {
                 // Exit
                 return;
             }
             // Reveal that tile in that X and Y coordinate
-            Tile tile = tiles[x, y, z];
+            Tile tile = tiles[x, y];
             // Get number of mines around that tile
             int adjacentMines = GetAdjacentMineCount(tile);
             // Reveal the tile
@@ -133,16 +124,12 @@ namespace Minesweeper3D
             if (adjacentMines == 0)
             {
                 // This tile has been visited
-                visited[x, y, z] = true;
+                visited[x, y] = true;
                 // Visit all other tiles around this tile
-                FFuncover(x - 1, y, z, visited);
-                FFuncover(x + 1, y, z, visited);
-
-                FFuncover(x, y - 1, z, visited);
-                FFuncover(x, y + 1, z, visited);
-
-                FFuncover(x, y, z - 1, visited);
-                FFuncover(x, y, z + 1, visited);
+                FFuncover(x - 1, y, visited);
+                FFuncover(x + 1, y, visited);
+                FFuncover(x, y - 1, visited);
+                FFuncover(x, y + 1, visited);
             }
         }
         // Scans the grid to check if there are no more empty tiles
@@ -155,18 +142,15 @@ namespace Minesweeper3D
             {
                 for (int y = 0; y < height; y++)
                 {
-                    for (int z = 0; z < depth; z++)
+                    Tile tile = tiles[x, y];
+                    // If tile is revealed or is a mine
+                    if (tile.isRevealed || tile.isMine)
                     {
-                        Tile tile = tiles[x, y, z];
-                        // If tile is revealed or is a mine
-                        if (tile.isRevealed || tile.isMine)
-                        {
-                            // Skip to next loop iteration
-                            continue;
-                        }
-                        // An empty tile has not been revealed
-                        emptyTileCount++;
+                        // Skip to next loop iteration
+                        continue;
                     }
+                    // An empty tile has not been revealed
+                    emptyTileCount++;
                 }
             }
             // Return true if all empty tiles have been revealed
@@ -180,15 +164,12 @@ namespace Minesweeper3D
             {
                 for (int y = 0; y < height; y++)
                 {
-                    for (int z = 0; z < depth; z++)
+                    Tile tile = tiles[x, y];
+                    // Check if tile is a mine
+                    if (tile.isMine)
                     {
-                        Tile tile = tiles[x, y, z];
-                        // Check if tile is a mine
-                        if (tile.isMine)
-                        {
-                            // Reveal that tile
-                            tile.Reveal();
-                        }
+                        // Reveal that tile
+                        tile.Reveal();
                     }
                 }
             }
@@ -212,9 +193,8 @@ namespace Minesweeper3D
             {
                 int x = selected.x;
                 int y = selected.y;
-                int z = selected.z;
                 // Use Flood Fill to uncover all adjacent mines
-                FFuncover(x, y, z, new bool[width, height, depth]);
+                FFuncover(x, y, new bool[width, height]);
             }
             // Are there no more empty tiles in the game at this point?
             if (NoMoreEmptyTiles())
@@ -228,7 +208,7 @@ namespace Minesweeper3D
 
         Tile GetHitTile(Vector2 mousePosition)
         {
-            Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray camRay = Camera.main.ScreenPointToRay(mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(camRay, out hit))
             {
@@ -247,7 +227,7 @@ namespace Minesweeper3D
                     SelectTile(hitTile);
                 }
             }
-            if(Input.GetMouseButtonDown(2))
+            if (Input.GetMouseButtonDown(2))
             {
                 Tile hitTile = GetHitTile(Input.mousePosition);
                 if (hitTile)
@@ -266,35 +246,29 @@ namespace Minesweeper3D
         void UpdateGrid()
         {
             // Store half the size of the grid
-            Vector3 halfSize = new Vector3(width * .5f, height * .5f, depth * .5f);
+            Vector2 halfSize = new Vector2(width * .5f, height * .5f);
 
             // Offset
-            Vector3 offset = new Vector3(.5f, .5f, .5f);
+            Vector2 offset = new Vector2(.5f, .5f);
 
             // Loop through the entire list of tiles
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
                 {
-                    for (int z = 0; z < depth; z++)
-                    {
-                        // Generate position for current tile
-                        Vector3 position = new Vector3(x - halfSize.x,
-                                                       y - halfSize.y,
-                                                       z - halfSize.z);
-                        // Offset position to center
-                        position += offset;
-                        // Apply spacing
-                        position *= spacing;
-                        // Spawn a new tile
-                        Tile tile = tiles[x, y, z];
-
-
-                        tile.transform.position = position;
-                    }
+                    // Generate position for current tile
+                    Vector2 position = new Vector2(x - halfSize.x,
+                                                   y - halfSize.y);
+                    // Offset position to center
+                    position += offset;
+                    // Apply spacing
+                    position *= spacing;
+                    // Spawn a new tile
+                    Tile tile = tiles[x, y];
+                    // Apply new position to Tile
+                    tile.transform.position = position;
                 }
             }
         }
     }
-
 }
